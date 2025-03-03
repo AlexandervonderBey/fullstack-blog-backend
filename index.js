e6c3af2f2c44f6e720a9a846c140f75ef0821351
Johanna <johanna.k.1996@googlemail.com>
2025-03-03 10:47:03 +0100

    connecting database via sequelize, full CRUD operations

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
-import pg from "pg";
+import {
+  createPost,
+  deletePost,
+  getPostById,
+  getPosts,
+  updatePost,
+} from "./controllers/posts.js";
+import "./db/index.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,7 +14,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // functions for different hht methods and routes
-app.get("/", (req, res) => {});
+// app.route() helps us define handlers for different HTTP methods on the same route
+app.route("/posts").get(getPosts).post(createPost);
+app.route("/posts/:id").get(getPostById).put(updatePost).delete(deletePost);
 
 app.listen(port, () => {
   console.log(`Server is running on  http://localhost:${port}`);
